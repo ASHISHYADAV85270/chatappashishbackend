fff865172b3f41727d159f3061d43f219d098a37
Ashish <savitayadavmansarover@gmail.com>
2023-07-11 23:09:21 +0530

    Done till login register and just selecting avatar

---
+++ server/routes/userRouter.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { LoginUser, RegisterUser, getprofile, setavatar } from '../controllers/userControl.js';
+
+const router = express();
+router.post('/register', RegisterUser); //api/auth/register
+router.post('/login', LoginUser); //api/auth/register
+router.post('/setavatar', setavatar);
+
+export default router;
