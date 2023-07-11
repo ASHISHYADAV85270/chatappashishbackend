4b368001883be3302ea7de012db55af60759abec
Ashish <savitayadavmansarover@gmail.com>
2023-07-12 01:48:39 +0530

    done till sending token

--- client/src/pages/Chat.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Chat = () => {
-  return <div className="text-red-400">Chat</div>;
-};
-
-export default Chat;
--- server/data/database.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-mongoose.set('strictQuery', false);
-export const connectDB = () => {
-    mongoose
-        .connect(process.env.MONGO_URL, { dbName: "chatappashish" })
-        .then((c) => { console.log(`data base connected `) })
-        .catch((err) => { console.log(err) });
-}
