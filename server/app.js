a8bca19b29582a8583092939bcc82dad4c2d50bb
Ashish Yadav <ashish.yadav@aquera.com>
2024-08-23 00:47:17 +0530

    added env file and configured path

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 config({
-    path: "./data/config.env"
+    path: ".env"
 });
 
 // app.use(cors()); //jis sai dusrai server sai excess kr skai 
